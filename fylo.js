37fc2fb3fe851c003fdc0ae4c25ea2ca19c440e3
Hualdo <hucastillo@gmail.com>
2022-05-23 15:22:51 -0500

    Script for email validation

--- fylo.js
+++ fylo.js
@@ -2,6 +2,9 @@ let work = document.getElementById('work')
 let works = document.getElementById('works')
 let a_work = document.getElementById('a_work')
 let hr = document.getElementById('hr_line')
+let email = document.getElementById('email')
+let button_submit = document.getElementById('button_submit')
+let error = document.getElementById('error')
 
 work.addEventListener('mouseover',()=>{
     a_work.style.color='white'
@@ -17,3 +20,13 @@ work.addEventListener('mouseout',()=>{
     hr.style.borderColor='hsl(176, 68%, 64%)'
     works.className='work' 
 })
+
+function pruebaemail (valor){
+	re=/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+	if(!re.exec(valor)){
+        error.className='show'
+    }
+    else{
+        error.className='close_error'
+    }
+}
